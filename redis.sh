1351765c10a7702da0ecf36e1f6170fa00dbe51c
surendra Babu C <surendra9703802865@gmail.com>
2023-12-22 11:03:45 +0530

    First Script

--- redis.sh
+++ redis.sh
@@ -1,15 +1,15 @@
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Install Redis repo file <<<<<<<<<<<<<<<<<<<<<</e[0m"
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Install Redis repo file <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+dnf install https://rpms.remirepo.net\enterprise/remi-release-8.rpm -y
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Enable redis 6.2 <<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Enable redis 6.2 <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 dnf module enable redis:remi-6.2 -y
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Install redis <<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Install redis <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 dnf install redis -y
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Update listen address from 127.0.0.1 to 0.0.0.0 <<<<<<<<<<<<<<<<<<<<<</e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|g' /etc/redis.conf /etc/redis/redis.conf
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Update listen address from 127.0.0.1 to 0.0.0.0 <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+sed -i -e 's|127.0.0.1|0.0.0.0|g' \etc/redis.conf \etc/redis/redis.conf
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> start redis <<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> start redis <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl enable redis
 systemctl restart redis
--- user.sh
+++ user.sh
@@ -1,40 +1,41 @@
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Enable nodejs:18 <<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Enable nodejs:18 <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Install nodejs <<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Install nodejs <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 dnf install nodejs -y
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Add app user <<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Add app user <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 useradd roboshop
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> create app directory <<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> create app directory <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Download app content <<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Download app content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> unzip app content <<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> unzip app content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/user.zip
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Install nodejs dependencies <<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Install nodejs dependencies <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 cd /app
 npm install
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> copy user service file <<<<<<<<<<<<<<<<<<<<<</e[0m"
-cp user.service /etc/systemd/system/user.service
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> copy user service file <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> start user <<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> start user <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable user
 systemctl start user
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> copy mongodb repo file <<<<<<<<<<<<<<<<<<<<<</e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> copy mongodb repo file <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>>Install mongodb client <<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>Install mongodb client <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 dnf install mongodb-org-shell -y
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Load Schema <<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Load Schema <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 mongo --host mongodb-dev.surendrababuc01.online </app/schema/user.js
