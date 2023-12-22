d3437f654f78a560770f260ba6245d4edaf65de7
surendra Babu C <surendra9703802865@gmail.com>
2023-12-22 09:54:27 +0530

    First Script

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,16 @@
+echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Disable mysql default version <<<<<<<<<<<<<<<<<<<<<</e[0m"
 dnf module disable mysql -y
+
+echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Copy mysql repo file <<<<<<<<<<<<<<<<<<<<<</e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Install mysql <<<<<<<<<<<<<<<<<<<<<</e[0m"
 dnf install mysql-community-server -y
+
+echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> start mysql <<<<<<<<<<<<<<<<<<<<<</e[0m"
 systemctl enable mysqld
 systemctl start mysqld
+
+echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> set mysql root password <<<<<<<<<<<<<<<<<<<<<</e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
 
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,18 @@
+echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Download erlang repo <<<<<<<<<<<<<<<<<<<<<</e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+
+echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Download rabbitmq repo file <<<<<<<<<<<<<<<<<<<<<</e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+
+echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> Install rabbitmq <<<<<<<<<<<<<<<<<<<<<</e[0m"
 dnf install rabbitmq-server -y
+
+echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> start rabbitmq <<<<<<<<<<<<<<<<<<<<<</e[0m"
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
+
+echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> add app user <<<<<<<<<<<<<<<<<<<<<</e[0m"
 rabbitmqctl add_user roboshop roboshop123
+
+echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>>> set_permissions of app user <<<<<<<<<<<<<<<<<<<<<</e[0m"
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
