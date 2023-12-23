23a088b9b9ab0e19f8be1e9bd09aa20694d64e19
surendra Babu C <surendra9703802865@gmail.com>
2023-12-23 21:29:21 +0530

    First Script

--- catalogue.sh
+++ catalogue.sh
@@ -1,38 +1,7 @@
 script_path=$(dirname $0)
 source ${script_path}/common.sh
 
-echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Enable nodejs:18 <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-
-echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Install nodejs <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-dnf install nodejs -y
-
-echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Add Application user <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-useradd ${app_user}
-
-echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Create app directory <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Download app content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-
-echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Unzip app content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-unzip /tmp/catalogue.zip
-
-echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Download nodejs dependencies <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-cd /app
-npm install
-
-echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Copy catalogue service file <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-cp ${script_path}/catalogue.service /etc/systemd/system/catalogue.service
-
-echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Start Catalogue <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
+func_nodejs
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Copy mongodb repo file <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
--- common.sh
+++ common.sh
@@ -1,1 +1,42 @@
 app_user=roboshop
+
+func_print_head() {
+   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> $1 <<<<<<<<<<<<<<<<<<<<<<\e[0m"
+}
+
+func_nodejs() {
+
+  func_print_head "Enable nodejs:18"
+  dnf module disable nodejs -y
+  dnf module enable nodejs:18 -y
+
+  func_print_head "Install nodej"s
+  dnf install nodejs -y
+
+  func_print_head "Add Application user"
+  useradd ${app_user}
+
+  func_print_head "Create app directory"
+  rm -rf /app
+  mkdir /app
+
+  func_print_head "Download app content"
+  curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+  cd /app
+
+  func_print_head "Unzip app content"
+  unzip /tmp/catalogue.zip
+
+  func_print_head "Download nodejs dependencie"s
+  cd /app
+  npm install
+
+  func_print_head "Copy catalogue service file"
+  cp ${script_path}/catalogue.service /etc/systemd/system/catalogue.service
+
+  func_print_head "Start Catalogue"
+  systemctl daemon-reload
+  systemctl enable catalogue
+  systemctl restart catalogue
+
+}
