e7b3c45c5e8538338466f21d7d1c7a09529662ab
surendra Babu C <surendra9703802865@gmail.com>
2023-12-24 16:51:12 +0530

    First Script

--- common.sh
+++ common.sh
@@ -9,7 +9,7 @@ func_print_head() {
 func_stat_check() {
   if [ $? -eq "0" ]
   then
-    echo -e "\e[32mSUCEESS\e[0m"
+    echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
     exit 1
