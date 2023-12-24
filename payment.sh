d05d423c661cd264bfd108c66eb82d8bba05718e
surendra Babu C <surendra9703802865@gmail.com>
2023-12-24 17:03:06 +0530

    First Script

--- payment.sh
+++ payment.sh
@@ -1,9 +1,10 @@
 script=$(realpath "$0")
 script_path=$(dirname ${script})
 source ${script_path}/common.sh
-rabbitmq_app_password=$1
+rabbitmq_appuser_password=$1
 
-if [ -z "$rabbitmq_appuser_password" ]; then
+if [ -z "$rabbitmq_appuser_password" ]
+ then
   echo Input Rabbitmq Appuser Password Missing
   exit 1
 fi
