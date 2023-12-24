30c8de00f66b19528cf7e69ad28cf66a88f28c91
surendra Babu C <surendra9703802865@gmail.com>
2023-12-25 01:17:59 +0530

    First Script

--- common.sh
+++ common.sh
@@ -15,6 +15,7 @@ func_stat_check() {
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
+    echo -e "\e[31mPlease refer /tmp/roboshop.log file for more information\e[0m"
     exit 1
   fi
 }
